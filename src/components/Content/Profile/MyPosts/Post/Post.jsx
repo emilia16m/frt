72ec272e171cb493a84e8d50385d00575e591e49
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-03 17:38:38 +0300

    Create Post.jsx

---
+++ src/components/Content/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import s from './Post.module.css'
+
+
+const Post = () => {
+    return (
+        <div>
+            <img className={s.item_img} src="https://best-fly.ru/wp-content/uploads/2020/08/ikonka-malchik.jpg" alt="" />
+            <span>Post number</span>
+        </div>
+    )
+}
+
+export default Post;
