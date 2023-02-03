4bf7499a1c12da78942ba75a371014ccefe1dfee
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-03 17:42:40 +0300

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () => {
+    return <header className={s.header}>
+    <img scr="https://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png" alt="logo" /> 
+  </header>
+}
+
+export default Header;
