8e66b0be409bd6ccf161d4ea16333da72e2915b4
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-03 17:45:38 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Profile from './components/Content/Profile/Profile';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <div className='app-wrapper' >
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
