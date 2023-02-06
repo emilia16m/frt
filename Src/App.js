60c9897380a13a0c97d91adf93d7126daae7408f
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-06 17:22:46 +0300

    Update and rename src/App.js to Src/App.js

---
+++ Src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Dialogs from './components/Content/Dialogs/Dialogs';
+import Music from './components/Content/Music/Music';
+import News from './components/Content/News/News';
+import Profile from './components/Content/Profile/Profile';
+import Settings from './components/Content/Settings/Settings';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='app-wrapper' >
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/dialogs' element={<Dialogs />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
