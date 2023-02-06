a3b873db67a97033b7c8327ac0e1d4f06197b61d
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-06 17:25:36 +0300

    Update Nav.jsx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Nav.module.css'
 
 const Nav = () => {
     return <nav className={s.nav}>
     <ul>
-      <li><a>Профиль</a></li>
-      <li><a>Сообщения</a></li>
-      <li><a>Новости</a></li>
-      <li><a>Музыка</a></li>
-      <li><a>Настройки</a></li>
+      <li><NavLink to='/profile'>Профиль</NavLink></li>
+      <li><NavLink to='/dialogs'>Сообщения</NavLink></li>
+      <li><NavLink to='/news'>Новости</NavLink></li>
+      <li><NavLink to='/music'>Музыка</NavLink></li>
+      <li><NavLink to='/settings'>Настройки</NavLink></li>
     </ul>
   </nav>
 }
 
 export default Nav;
+ 
