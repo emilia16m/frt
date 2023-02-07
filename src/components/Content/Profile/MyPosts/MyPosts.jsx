d7ceb4b8bbd8e87e1d632554265a760f815e1a9d
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-07 10:51:38 +0300

    Update MyPosts.jsx

--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,13 @@
 import React from 'react'; 
 import Post from './Post/Post';
-
+import s from './MyPosts.module.css'
 const MyPosts = () => {
     return(
-        <div>
-            My post
-            <div>
+        <div className={s.posts_wrapper}>
+            <span>My post</span>
+            <div className={s.add_post}>
                 <textarea></textarea>
-                <button>Add post</button>
+                <button className={s.button_post}>Add post</button>
             </div>
             <Post />
         </div>
