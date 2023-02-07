ecef35378f7c572a68b6b112270270d7070497a3
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-07 18:15:37 +0300

    Update Dialogs.jsx

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -15,19 +15,30 @@ const Message = (props) =>{
     return <div className={s.messages_item}>{props.message}</div>
 }
 
+let dialogsData =[
+    {id: 1, name: "Olivia"},
+    {id: 2, name: "Rosa"},
+    {id: 3, name: "Stefany"},
+    {id: 4, name: "Laya"},
+]
+let dialogsItem = dialogsData.map( name_item => <DialogItem name={name_item.name} id={name_item.id}/>)
+
+
+let messageDate =[
+    {id: 1, message: "Hi"},
+    {id: 2, message: "How are you"},
+    {id: 3, message: "Im fine"},
+]
+let messageItem = messageDate.map(item => <Message message={item.message}/>)
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialog_name}>
-                <DialogItem name ="Alex" id="1"/>
-                <DialogItem name ="Olivia" id="2"/>
-                <DialogItem name ="Stefan" id="3"/>
-                <DialogItem name ="Demon" id="4"/>
+                {dialogsItem}
             </div>
             <div className={s.dialog_messages}>
-                <Message message="Hi"/>
-                <Message message="How are you"/>
-                <Message message="Im fine"/>
+                {messageItem}
             </div>
         </div>
     )
