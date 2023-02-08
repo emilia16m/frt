33ff72d7cf5442c7a4711c1c6782c392c2175065
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-08 17:52:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Settings from './components/Content/Settings/Settings';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <div className='app-wrapper' >
@@ -18,7 +18,7 @@ function App() {
         <div className='app-wrapper-content'>
           <Routes>
             <Route path='/profile/*' element={<Profile />} />
-            <Route path='/dialogs/*' element={<Dialogs />} />
+            <Route path='/dialogs/*' element={<Dialogs dialogs={props.appState.dialogs}  message={props.appState.message}/>} />
             <Route path='/music/*' element={<Music />} />
             <Route path='/news/*' element={<News />} />
             <Route path='/settings/*' element={<Settings />} />
