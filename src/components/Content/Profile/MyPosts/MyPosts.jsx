940975b16de37761c7d805d19fd3712356e55a6f
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-08 17:51:45 +0300

    Update MyPosts.jsx

--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,25 @@
 import React from 'react'; 
 import Post from './Post/Post';
 import s from './MyPosts.module.css'
+
+
+
+
 const MyPosts = () => {
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        const text = newPostElement.current.value;
+        alert(text)
+    }
+
     return(
         <div className={s.posts_wrapper}>
             <span>My post</span>
             <div className={s.add_post}>
-                <textarea></textarea>
-                <button className={s.button_post}>Add post</button>
+                <textarea ref={newPostElement}></textarea>
+                <button className={s.button_post} onClick={ addPost } > Add post </button>
             </div>
             <Post />
         </div>
