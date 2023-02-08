88821396d63fe2b4ee903e1ee60d45c9f040b790
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-08 17:53:48 +0300

    Create state.js

---
+++ src/redux/state.js
@@ -0,0 +1,13 @@
+let state = {
+    dialogs: [
+        {id: 1, name: "Olivia"},
+        {id: 2, name: "Rosa"},
+        {id: 3, name: "Stefany"},
+        {id: 4, name: "Laya"}],
+    message: [
+        {id: 1, message: "Hi"},
+        {id: 2, message: "How are you"},
+        {id: 3, message: "Im fine"}]
+}
+
+export default state;
