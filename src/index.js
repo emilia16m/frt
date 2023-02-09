23a821ac9e0b7fe8a3a44c5c8b73a598a7e79da0
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-09 17:35:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state.js';
+import { rerenderEntireTree } from './redux/render';
+
+rerenderEntireTree(state)
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App appState={state}/>
-  </React.StrictMode>
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
