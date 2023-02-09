71676af2da970569a80886c4ffe43443582500d0
emilia16m <73434780+emilia16m@users.noreply.github.com>
2023-02-09 17:35:12 +0300

    Update state.js

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "./render"
+
 let state = {
     dialogs: [
         {id: 1, name: "Olivia"},
@@ -8,6 +10,18 @@ let state = {
         {id: 1, message: "Hi"},
         {id: 2, message: "How are you"},
         {id: 3, message: "Im fine"}]
+}
+
+
+
+export let addMessage = (Message) => {
+    let newMessage = {
+        id: 4,
+        message: Message
+    }
+
+    state.message.push(newMessage)
+    rerenderEntireTree(state)
 }
 
 export default state;
